65d5bd9470779fca718460fd66044974230c3243
zzx <906596387@qq.com>
2021-09-16 14:52:57 +0800

    创建user类

---
+++ testUser/src/com/zzx/pojo/User.java
@@ -0,0 +1,11 @@
+package com.zzx.pojo;
+
+/**
+ * @Author: zzx
+ * @Date: 2021/9/16 - 09 - 16 - 14:51
+ * @Description: com.zzx.pojo
+ * @version: 1.0
+ */
+public class User {
+    private Integer age;
+}
